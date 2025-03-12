b9600ef79ef474aa9f0f10fe1b149d898948c755
SupriyaGitUser <supriyabhosale5@gmail.com>
2025-03-12 18:52:20 +0530

    my first commit

---
+++ src/practice/Introduction.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Webdriver driver = new ChromeDriver();
+
+	}
+
+}
