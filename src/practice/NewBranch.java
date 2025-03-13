8c1afbc4789d76423d2851dd8ca5c2ec1ad399a2
SupriyaGitUser <supriyabhosale5@gmail.com>
2025-03-13 14:59:08 +0530

    commit for new branch

---
+++ src/practice/NewBranch.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class NewBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("New branch created develope");
+
+	}
+
+}
